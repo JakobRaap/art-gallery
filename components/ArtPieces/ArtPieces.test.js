d9774c77b235beaeb40aeafd8e859e866b656a88
Jakob Raap <bokaj.paar@gmail.com>
2023-04-20 16:12:48 +0200

    tests started

---
+++ components/ArtPieces/ArtPieces.test.js
@@ -0,0 +1,26 @@
+import ArtPieces from ".";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const exampleArray = [
+  {
+    artist: "Maksym",
+    imageSource:
+      "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+    name: "Maksyms beautiful picture",
+  },
+  {
+    artist: "Jakob",
+    imageSource:
+      "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+    name: "Jakobs beautiful picture",
+  },
+];
+
+test("All art pieces are displayed as a list ", () => {
+  render(<ArtPieces pieces={exampleArray} />);
+
+  const pictureName = screen.getByRole("paragraph", {
+    name: "Maksyms beautiful picture",
+  });
+});
--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,10 +1,13 @@
 import ArtPiecePreview from "../ArtPiecePreview";
+
 export default function ArtPieces({ pieces }) {
   console.log(pieces);
+
   return (
     <ul>
       {pieces.map((piece) => (
         <li key={piece.slug}>
+          {" "}
           <ArtPiecePreview
             image={piece.imageSource}
             title={piece.name}
